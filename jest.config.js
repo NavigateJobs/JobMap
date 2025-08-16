2fe7668357cd36ec1f74fbfff307621221d13bf2
Ninodi <ninomachaidze16@gmail.com>
2025-08-17 00:27:05 +0400

    chore: setup commitlint for husky

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native|@react-native|nativewind|react-native-worklets)"
+  ],
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/__mocks__/styleMock.js'
+  }
 };
